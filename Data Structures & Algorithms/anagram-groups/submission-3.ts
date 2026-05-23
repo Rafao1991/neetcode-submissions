d71bfb341fe5b1c2171bca9b9df5aae79271ef36
Rafael Sousa <1c302489@gmail.com>
2026-05-23 12:46:47 -0300

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const ASCII_a = 'a'.charCodeAt(0);
+        const map: Record<string, string[]> = {};
+
+        strs.forEach((str) => {
+            const count = new Array<number>(26).fill(0);
+
+            for (const c of str)
+                count[c.charCodeAt(0) - ASCII_a] += 1;
+
+            const key = count.join('|');
+
+            if (map[key] === undefined) map[key] = [];
+
+            map[key].push(str);
+        });
+
+        return [...Object.values(map)]
+    }
+}
