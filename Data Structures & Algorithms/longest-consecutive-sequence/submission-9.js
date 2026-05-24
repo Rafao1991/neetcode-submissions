4fc6f4f90ec719d75113a37cea5fafd68f48110d
Rafael Sousa <1c302489@gmail.com>
2026-05-24 19:21:30 -0300

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums);
+        let longestLen = 0;
+        
+        for (const num of nums) {           
+            // is num start
+            if (set.has(num - 1)) continue;
+
+            let length = 1
+            while (set.has(num + length)) length++;
+            longestLen = Math.max(longestLen, length);
+        }
+
+        return longestLen;
+    }
+}
