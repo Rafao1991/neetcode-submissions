01583ddd4203997befc0533247ec711b3c8712a5
Rafael Sousa <1c302489@gmail.com>
2026-06-17 17:05:37 -0300

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        const map = new Map<string, number>();
+        let result = 0;
+        let l = 0;
+        for (let r = 0; r < s.length; r++) {
+            map.set(s[r], (map.get(s[r]) ?? 0) + 1)
+            
+            while (r - l + 1 - Math.max(...(map.values())) > k) {
+                map.set(s[l], map.get(s[l]) - 1);
+                l++
+            }
+
+            result = Math.max(result, r - l + 1);
+        }
+
+        return result;
+    }
+}
